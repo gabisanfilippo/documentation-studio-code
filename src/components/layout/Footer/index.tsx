0aa5a05713d610d56d9a2d63749075c756223300
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2023-12-23 14:32:09 -0300

    feat: footer

---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,67 @@
+import {
+  AlertIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BranchIcon,
+  ErrorIcon,
+  NotificationIcon,
+  PersonDialogBoxIcon,
+  Synchronous,
+} from "@/assets/icons";
+import { ReactNode } from "react";
+
+const TextContainer = ({ children }: { children: ReactNode }) => {
+  return <li className="px-4 flex items-center gap-1">{children}</li>;
+};
+
+export const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-screen h-5 bg-blue-06 text-xs flex justify-between items-center text-base-01">
+      <ul className="flex items-center">
+        <TextContainer>
+          <BranchIcon />
+          {/* TODO: route name as branch */}
+          <p>main</p>
+        </TextContainer>
+        <TextContainer>
+          <Synchronous />
+          <p className="ml-1">0</p>
+          <ArrowDownIcon />
+          <p>1</p>
+          <ArrowUpIcon />
+        </TextContainer>
+        <TextContainer>
+          <ErrorIcon />
+          <p>0</p>
+          <AlertIcon />
+          <p>0</p>
+        </TextContainer>
+      </ul>
+      <ul className="pr-4 flex items-center gap-8">
+        <li>
+          <p>Ln 17, Col 3</p>
+        </li>
+        <li>
+          <p>Spaces: 2</p>
+        </li>
+        <li>
+          <p>UTF-8</p>
+        </li>
+        <li>
+          <p>LF</p>
+        </li>
+        <li>
+          <p>TypeScript</p>
+        </li>
+        <li>
+          {/* TODO: about me */}
+          <PersonDialogBoxIcon />
+        </li>
+        <li>
+          {/* TODO: notification */}
+          <NotificationIcon />
+        </li>
+      </ul>
+    </footer>
+  );
+};
