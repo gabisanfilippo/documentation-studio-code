0492a81ae84847d8914b0d0137eaf86bb6989f98
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2023-12-24 15:37:36 -0300

    feat: branch menu

---
+++ src/components/layout/Menu/Branch/index.tsx
@@ -0,0 +1,12 @@
+import { DotsMenuIcon } from "@/assets/icons";
+
+export const BranchMenu = () => {
+  return (
+    <section className="w-300px bg-base-19 flex flex-col gap-2 text-base-06 px-2 ">
+      <div className="px-2 py-4 text-xs flex justify-between items-center">
+        <p>SOURCE CONTROL</p>
+        <DotsMenuIcon />
+      </div>
+    </section>
+  );
+};
