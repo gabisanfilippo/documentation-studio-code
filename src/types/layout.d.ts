5e0e6970b8bb28ab616e096fc8eff3996a943338
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2023-12-24 12:05:41 -0300

    feat: layout types

---
+++ src/types/layout.d.ts
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+
+interface ILayoutContext {
+  iconSelected: NameIconType;
+  setIconSelected: Dispatch<SetStateAction<NameIconType>>;
+}
+
+type NameIconType =
+  | "files"
+  | "search"
+  | "branch"
+  | "debug"
+  | "extensions"
+  | "profile"
+  | "configurations";
+
+interface IMenuInfo {
+  icon: ReactNode;
+  path: string;
+  name: NameIconType;
+}
