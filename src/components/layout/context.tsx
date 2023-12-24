7f6a19ad8b6842b92ecc6f22f6371478708057b6
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2023-12-24 12:05:27 -0300

    feat: menu context

---
+++ src/components/layout/context.tsx
@@ -0,0 +1,20 @@
+import { IChildren } from "@/types/commom";
+import { ILayoutContext, NameIconType } from "@/types/layout";
+import { createContext, useState } from "react";
+
+const defaultValues: ILayoutContext = {
+  iconSelected: "files",
+  setIconSelected: () => {},
+};
+
+export const LayoutContext = createContext<ILayoutContext>(defaultValues);
+
+export const LayoutContextProvider = ({ children }: IChildren) => {
+  const [iconSelected, setIconSelected] = useState<NameIconType>("files");
+
+  return (
+    <LayoutContext.Provider value={{ iconSelected, setIconSelected }}>
+      {children}
+    </LayoutContext.Provider>
+  );
+};
